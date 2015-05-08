7fa50c0857727dcfb061930227cff6e473e85336
Stephen Sherratt <steve@stevebob.net>
2015-05-09 04:41:04 +1000

    Unseeing cells after they leave the field of vision.

--- src/behaviour/behaviour.cpp
+++ src/behaviour/behaviour.cpp
@@ -3,6 +3,13 @@
 #include "actor/character.hpp"
 
 void behaviour::observe_world(const world &w) {
+    for (std::vector<game_cell*>::iterator it = visible_cells_.begin(); 
+        it != visible_cells_.end(); ++it) {
+        game_cell *c = *it;
+        
+        knowledge_grid_.get_cell(c->coord).unsee();
+    }
+
     visible_cells_.clear();
     fov_.push_visible_cells(character_.position, visible_cells_);
 
